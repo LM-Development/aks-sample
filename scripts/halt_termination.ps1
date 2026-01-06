6bf0b03c96180b5c56c6022d3b86923973e41619
InDieTasten <indietasten@gmail.com>
2026-01-06 16:02:27 +0100

    feat: halt termination logging
    Refs: AB#40148

--- scripts/halt_termination.ps1
+++ scripts/halt_termination.ps1
@@ -6,20 +6,25 @@ while($continue)
 {
     try
     {
+        Write-Host "Calling endpoint to check for active calls..."
         $result = Invoke-WebRequest -Uri "http://localhost:$CallSignalingPort2/calls" -UseBasicParsing
+
+        Write-Host "Response content: $($result.Content)"
         $calls = $result.Content | ConvertFrom-Json
 
         if ($calls.Count -gt 0)
         {
+            Write-Host "Active calls detected. Halting termination."
             Start-Sleep -Seconds 60
         }
         else
         {
+            Write-Host "No active calls. Proceeding with termination."
             $continue = $false
         }
     }
     catch
     {
-        "Error while calling endpoint."
+        Write-Host "Error while calling endpoint."
     }
 }
